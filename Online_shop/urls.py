d240b03430d9a5a5577c9645133cb92ca6430a31
mohamadreza <mohamadrezabjr@gmail.com>
2025-05-31 22:46:00 +0330

    Serve media

--- Online_shop/urls.py
+++ Online_shop/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -37,3 +40,10 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
