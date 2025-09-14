e20e523ca86060f10723d9577d4166be0c0587b8
theboss878787 <bjr.mohamadreza@gmail.com>
2025-09-14 17:33:06 +0330

    Update settings.py

--- Online_shop/settings.py
+++ Online_shop/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-ENVIRONMENT = str(os.getenv('ENVIRONMENT', defualt ='production'))
+ENVIRONMENT = str(os.getenv('ENVIRONMENT', default ='production'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == 'production':
